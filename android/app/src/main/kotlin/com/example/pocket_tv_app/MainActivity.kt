2ffff5dd9ee02750921a83121de2e8a68e1f652e
K123Ritesh <riteshkumarlsarai@gmail.com>
2024-01-04 22:35:17 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/pocket_tv_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pocket_tv_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:pocket_tv_app/screens/nav_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pocket Tv',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: NavScreen(),
+    );
+  }
+}
